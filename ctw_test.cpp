5fdd495b661bd1d681be6e1d6867f5f4370a3374
Joshua Nelson <cyberdash@gmail.com>
2012-10-29 19:58:27 +1100

    Apparently it just needed more training.

--- ctw_test.cpp
+++ ctw_test.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
 	size_t ct_size = 4;
 	ContextTree *ctw = new ContextTree(ct_size);
 	//Runs a coinflip example if the agent guesses randomly and the coin is random
-	for(int i = 0; i < 200; i++) {
+	for(int i = 0; i < 3000; i++) {
 	    int guess = rand01() < 0.5 ? 1 : 0;
 	    int coin = rand01() < 0.5 ? 1 : 0;
 		ctw->update(guess);
@@ -19,10 +19,10 @@ int main(int argc, char *argv[]) {
 		ctw->update(coin==guess ? 1 : 0);
 	}
 	
-	//Action and observation are 1, 0 so ctw_tree would predict reward 0 if it "understood" the game
-	ctw->update(1);
+	//Action and observation are 0, 1 so ctw_tree would predict reward 0 if it "understood" the game
 	ctw->update(0);
+	ctw->update(1);
 	std::cout << ctw->prettyPrint();
-	std::cout << "Sequence: " << ctw->printHistory() << std::endl; //Actually predicts 1...
+	std::cout << "Sequence: " << ctw->printHistory() << std::endl;
 	std::cout << "Next: "<< ctw->predictNext() << std::endl;
 }
