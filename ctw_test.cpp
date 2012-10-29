57e1ac7f631298d22eb59e8600d1cb85d7c40d74
Joshua Nelson <cyberdash@gmail.com>
2012-10-29 17:32:26 +1100

    Changed the test to refine where the problem is
    I suspect the issue is in the ctw_tree's update or predict methods. See ctw_tree for explanation of what's wrong

--- ctw_test.cpp
+++ ctw_test.cpp
@@ -10,15 +10,19 @@
 int main(int argc, char *argv[]) {
 	size_t ct_size = 4;
 	ContextTree *ctw = new ContextTree(ct_size);
-	for(int i = 0; i < 20; i++) {
-		ctw->update(1); //This is the symbol it will try to guess
-		ctw->update(1);
-		ctw->update(0);
-		ctw->update(0);
+	//Runs a coinflip example if the agent guesses randomly and the coin is random
+	for(int i = 0; i < 200; i++) {
+	    int guess = rand01() < 0.5 ? 1 : 0;
+	    int coin = rand01() < 0.5 ? 1 : 0;
+		ctw->update(guess);
+		ctw->update(coin);
+		ctw->update(coin==guess ? 1 : 0);
 	}
-	//ctw->update(0);
-	//ctw->revert();
+	
+	//Action and observation are 1, 0 so ctw_tree would predict reward 0 if it "understood" the game
+	ctw->update(1);
+	ctw->update(0);
 	std::cout << ctw->prettyPrint();
-	std::cout << "Sequence: " << ctw->printHistory();
+	std::cout << "Sequence: " << ctw->printHistory() << std::endl; //Actually predicts 1...
 	std::cout << "Next: "<< ctw->predictNext() << std::endl;
 }
